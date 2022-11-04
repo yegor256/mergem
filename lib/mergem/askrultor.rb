6bbfe21dc47bb632e7ad81c84e46ad0d3d3d3889
Yegor Bugayenko <yegor256@gmail.com>
2022-11-04 08:43:49 +0300

    #1 AskRultor

---
+++ lib/mergem/askrultor.rb
@@ -0,0 +1,44 @@
+# Copyright (c) 2022 Yegor Bugayenko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Ask Rultor to merge a pull request.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2022 Yegor Bugayenko
+# License:: MIT
+class Mergem::AskRultor
+  def initialize(api, loog)
+    @api = api
+    @loog = loog
+  end
+
+  def ask(repo, num)
+    begin
+      user = @api.user[:login]
+    rescue Octokit::Unauthorized
+      user = ''
+      @loog.debug('You are not using GitHub token...')
+    end
+    json = @api.issue_comments(repo, num)
+    @loog.debug("Found #{json.count} comments in #{repo}##{num}")
+    return false unless json.find { |j| j[:user][:login] == user }.nil?
+    @api.add_comment(repo, num, '@rultor please, try to merge')
+    true
+  end
+end
---
+++ test/test_askrultor.rb
@@ -0,0 +1,40 @@
+# Copyright (c) 2022 Yegor Bugayenko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'minitest/autorun'
+require 'octokit'
+require 'loog'
+require_relative '../lib/mergem/askrultor'
+
+# Test for AskRultor.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2022 Yegor Bugayenko
+# License:: MIT
+class TestAskRultor < Minitest::Test
+  def test_real
+    api = Octokit::Client.new
+    m = Mergem::AskRultor.new(api, Loog::VERBOSE)
+    asked = m.ask('yegor256/mergem', 1)
+    assert(!asked)
+  rescue Octokit::TooManyRequests => e
+    puts e.message
+    skip
+  end
+end
